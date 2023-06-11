17e5b00cb1497a8a5b83421b4a2f1478221ecbe0
Brisstone <okolijohnson69@gmail.com>
2023-06-11 13:55:22 +0100

    gitignore-restaged

--- configs/env.go
+++ configs/env.go
@@ -2,19 +2,19 @@ package configs
 // a helper function to load the environment variable 
 // using the github.com/joho/godotenv library installed
 import (
-    "log"
+    // "log"
     "os"
-    "github.com/joho/godotenv"
+    // "github.com/joho/godotenv"
 )
 
 // EnvMongoURI function that checks if the environment variable is correctly loaded 
 // and returns the environment variable.
 
 func EnvMongoURI() string {
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file", err)
-    }
+    // err := godotenv.Load()
+    // if err != nil {
+    //     log.Fatal("Error loading .env file", err)
+    // }
 
     return os.Getenv("MONGOURI")
 }
